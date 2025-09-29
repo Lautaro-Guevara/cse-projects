b7028e4d29a24b52fde9ef03a687fe4e77ec1d26
Lautaro <lfguevara2001@gmail.com>
2025-09-29 15:55:29 -0300

    Scripture memorizer in progress

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,58 @@
 public class Scripture
 {
-    private
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] wordArray = text.Split(" ");
+
+        foreach (string word in wordArray)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        int hiddenCount = 0;
+        int totalWords = _words.Count;
+
+        while (hiddenCount < numberToHide)
+        {
+            int index = random.Next(totalWords);
+            if (!_words[index].IsHidden()) ;
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = _reference.GetDisplayText() + "\n";
+        foreach (Word word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
