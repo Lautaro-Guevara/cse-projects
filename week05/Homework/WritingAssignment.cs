c4cdd132a5e588065fd85f0e8e849b0678101e5e
Lautaro-Guevara <lfguevara2001@gmail.com>
2025-09-29 09:47:39 -0300

    Homework learning activity done

---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,17 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{GetSummary()} - {_title} by {GetStudentName()}";
+    }
+}
+    
+    
