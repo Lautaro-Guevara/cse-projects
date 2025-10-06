13fd300c1d110b39f6b57e080661f1b937de07eb
Lautaro <lfguevara2001@gmail.com>
2025-10-06 15:52:24 -0300

    Mindfulness Activity in progress

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,75 @@
+public class Activity()
+{
+    public string _name;
+    public string _description;
+    public int _duration;
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Welcome to the {_name}");
+        Console.WriteLine();
+        Console.WriteLine(_description);
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        string durationString = Console.ReadLine();
+
+        _duration = int.Parse(durationString);
+
+        Console.WriteLine("Get ready...");
+        ShowSpinner(5);
+        Console.WriteLine();
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("Well done!!");
+        Console.WriteLine();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name}");
+        ShowSpinner(5);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        List<string> animationString =
+        [
+            "|",
+            "/",
+            "—",
+            "\\",
+            "|",
+            "/",
+            "—",
+            "\\",
+        ];
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string s = animationString[i];
+            Console.Write(s);
+            Thread.Sleep(500);
+            Console.Write("\b \b");
+            i++;
+
+            if (i >= animationString.Count)
+            {
+                i = 0;
+            }
+        }
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+
+}
