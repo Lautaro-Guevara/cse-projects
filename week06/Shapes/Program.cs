3f91547324bc47aef944bb4a7f3dc210899d6a1a
Lautaro <lfguevara2001@gmail.com>
2025-10-13 14:56:53 -0300

    Shapes activity completed

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 4),
+            new Rectangle("Blue", 3, 5),
+            new Circle("Green", 2)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"A {shape.GetColor()} shape with area {shape.GetArea()}");
+        }
     }
 }
